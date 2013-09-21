d5e997a6dfda5fbeb57f25d53c9143cee188c87f
wholalotta <ardaaskin@gmail.com>
2013-09-21 16:04:22 +0300

    .

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -47,7 +47,92 @@ $(document).on({
     }  
 }, '.jump');
 
+$("#tr_small_logo").mouseover(function() {	
+	if(!$(this).hasClass("clicked")){
+		$('#tr_small_logo').toggle(false);
+		$('#tr_big_logo').toggle(true);
+	}
+});
+
+$('#tr_big_logo').mouseout(function() {
+	if(!$(this).hasClass("clicked")){
+		$('#tr_small_logo').toggle(true);
+		$('#tr_big_logo').toggle(false);
+	}
+});
+
+$("#vi_small_logo").mouseover(function() {
+	if(!$(this).hasClass("clicked")){
+		$('#vi_small_logo').toggle(false);
+		$('#vi_big_logo').toggle(true);
+	}
+});
+
+$('#vi_big_logo').mouseout(function() {
+	if(!$(this).hasClass("clicked")){
+		$('#vi_small_logo').toggle(true);
+		$('#vi_big_logo').toggle(false);
+	}
+});
+
+$("#ba_small_logo").mouseover(function() {
+	if(!$(this).hasClass("clicked")){
+		$('#ba_small_logo').toggle(false);
+		$('#ba_big_logo').toggle(true);
+	}
+});
+
+$('#ba_big_logo').mouseout(function() {
+	if(!$(this).hasClass("clicked")){
+		$('#ba_small_logo').toggle(true);	
+		$('#ba_big_logo').toggle(false);
+	}
+});
+
 var acHeight = $('#accordionContainer').height();
-$('#accordion-slider>li .i').height(acHeight+150);
+$('#accordion-slider>li .i').height(acHeight+70);
+
+
+$(document).on("click", ".countryIcons", function(event){
+	var country = $(this).attr('data-type');
+	$(this).toggleClass("clicked");
+	if(country == "ban"){		
+		$('#tr_small_logo').toggle(true);
+		$('#tr_big_logo').toggle(false);
+		$('#vi_small_logo').toggle(true);
+		$('#vi_big_logo').toggle(false);
+		
+		$('#turkey').toggle(false);
+		$('#vietnam').toggle(false);
+		$('#bangladesh').toggle(true);
+		
+		$('#facilitiesBottomTextContainer').toggle(true);
+		$('#accordionContainer').css("height","50%");
+	}
+	else if(country == "tr"){
+		$('#ba_small_logo').toggle(true);
+		$('#ba_big_logo').toggle(false);
+		$('#vi_small_logo').toggle(true);
+		$('#vi_big_logo').toggle(false);
+		
+		$('#vietnam').toggle(false);
+		$('#bangladesh').toggle(false);
+		$('#turkey').toggle(true);
+		$('#facilitiesBottomTextContainer').toggle(true);
+		$('#accordionContainer').css("height","50%");
+	}
+	else if(country == "viet"){
+		$('#ba_small_logo').toggle(true);
+		$('#ba_big_logo').toggle(false);
+		$('#tr_small_logo').toggle(true);
+		$('#tr_big_logo').toggle(false);
+		
+		$('#turkey').toggle(false);
+		$('#bangladesh').toggle(false);
+		$('#vietnam').toggle(true);
+		$('#facilitiesBottomTextContainer').toggle(true);
+		$('#accordionContainer').css("height","50%");
+	}
+});
 
 });
